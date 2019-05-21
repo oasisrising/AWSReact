cd38325fd9e4772e48091b32dbe1ee7d6645a888
Michele Olsen <oasis.rising@gmail.com>
2019-05-21 09:19:35 -0500

    added some more text Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Part 2.
+          Created by Michele Olsen.
         </p>
         <a
           className="App-link"
